955e27f05642e7ab4e956b153b24ec4c958fa8f2
Shurimian <59806513+Shurimian@users.noreply.github.com>
2020-04-12 19:03:59 -0400

    Added functions for declaring & initializing

--- matrixSolver.cpp
+++ matrixSolver.cpp
@@ -5,28 +5,18 @@
 
 using namespace std;
 
-int main()
+//Declares the matrix
+void declare(int row, int column, int **array)
 {
-    int row{};
-    int column{};
-
-    cout << "Enter a nxm (row by column) matrix size: ";
-    cin >> row >> column;
-
-    int **array{new int *[row]};
     for (int i{}; i < row; ++i)
     {
         array[i] = new int[column];
     }
+}
 
-    for (int rowI{}; rowI < row; ++rowI)
-    {
-        for (int columnI{}; columnI < column; ++columnI)
-        {
-            array[rowI][columnI] = 0;
-        }
-    }
-
+//Input matrix values
+void initialize(int row, int column, int **array)
+{
     for (int rowI{}; rowI < row; ++rowI)
     {
         cout << "Enter the values in row " << rowI + 1 << ": ";
@@ -36,7 +26,79 @@ int main()
             cin >> array[rowI][columnI];
         }
     }
+}
 
+int main()
+{
+    int row{};
+    int column{};
+
+    //Get size of matrix
+    cout << "Enter a nxm (row by column) matrix size: ";
+    cin >> row >> column;
+
+    //Create a matrix to send to declaration function
+    int **array{new int *[row]};
+
+    declare(row, column, array);
+    initialize(row, column, array);
+
+    //Decide on what to do with matrix
+    char symb{};
+    cout << "Enter a matrix operation you would like to perform (+, -, *, RREF): ";
+    cin >> symb;
+
+    //2nd matrix for operations
+    if (symb == '*')
+    {
+        row = column;
+        cout << "Enter the columns in your 2nd matrix: ";
+        cin >> column;
+
+        int **array2{new int *[row]};
+
+        declare(row, column, array2);
+        initialize(row, column, array2);
+    }
+    else
+    {
+        int **array2{new int *[row]};
+
+        declare(row, column, array2);
+        initialize(row, column, array2);
+    }
+
+    switch (symb)
+    {
+        case '+':
+        {
+            for (int rowI{}; rowI < row; ++rowI)
+            {
+                for (int columnI{}; columnI < column; ++columnI)
+                {
+                    array[rowI][columnI] += array2[rowI][columnI];
+                }
+            }
+            break;
+        }
+        case '-':
+        {
+            for (int rowI{}; rowI < row; ++rowI)
+            {
+                for (int columnI{}; columnI < column; ++columnI)
+                {
+                    array[rowI][columnI] -= array2[rowI][columnI];
+                }
+            }
+            break;
+        }
+        case '*':
+        {
+        }
+    }
+
+    //Display matrix
+    //The addresses of the matrix go ROW BY ROW i.e. First row finishes, then 2nd row starts, etc. NOT column by column
     for (int rowI{}; rowI < row; ++rowI)
     {
         for (int columnI{}; columnI < column; ++columnI)
@@ -46,11 +108,11 @@ int main()
         cout << '\n';
     }
 
+    //Procedure to delete matrix & return memory to system.
     for (int i{}; i < row; ++i)
     {
         delete[] array[i];
     }
-
     delete[] array;
 
     return 0;
